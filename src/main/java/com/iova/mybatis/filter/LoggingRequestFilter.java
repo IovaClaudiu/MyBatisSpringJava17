1fed9018a94434c8d28edf992137e7e36adbd6eb
Claudiu IOVA <claudiu.iova@KRO.LOCAL>
2023-01-16 10:19:47 +0200

    Improve Logging Request Filter

--- src/main/java/com/iova/mybatis/filter/LoggingRequestFilter.java
+++ src/main/java/com/iova/mybatis/filter/LoggingRequestFilter.java
@@ -11,27 +11,48 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-@Order(value = Ordered.HIGHEST_PRECEDENCE)
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @WebFilter(filterName = "LoggingRequestFilter", urlPatterns = "/api/*")
 public class LoggingRequestFilter extends OncePerRequestFilter {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(LoggingRequestFilter.class);
 
     @Override
-    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        LOGGER.info("Request Info: [" + request.getMethod() + ", " + request.getRequestURI() + "]");
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
 
-        final CachedBodyHttpServletRequest cachedBodyHttpServletRequest = new CachedBodyHttpServletRequest(request);
-        final byte[] content = cachedBodyHttpServletRequest.getInputStream().readAllBytes();
-        if (content.length > 0)
-            LOGGER.info("Payload: " + new String(content, StandardCharsets.UTF_8));
+        filterChain.doFilter(requestWrapper, responseWrapper);
 
-        filterChain.doFilter(cachedBodyHttpServletRequest, response);
+        logRequest(requestWrapper);
+
+        logResponse(requestWrapper, responseWrapper);
+
+        responseWrapper.copyBodyToResponse();
     }
+
+    private void logRequest(ContentCachingRequestWrapper requestWrapper) {
+        LOGGER.info("Request Info: [ {} , {} ]", requestWrapper.getMethod(), requestWrapper.getRequestURI());
+
+        final byte[] content = requestWrapper.getContentAsByteArray();
+        if (content.length > 0)
+            LOGGER.info("Payload: {}", new String(content, StandardCharsets.UTF_8));
+    }
+
+    private void logResponse(ContentCachingRequestWrapper requestWrapper, ContentCachingResponseWrapper responseWrapper) {
+        LOGGER.info("Response Info: [ method= {}, status={} ]", requestWrapper.getMethod(), responseWrapper.getStatus());
+        
+        final byte[] responseContent = responseWrapper.getContentAsByteArray();
+        if (responseContent.length > 0)
+            LOGGER.info("Response body: [ {} ]", new String(responseContent, StandardCharsets.UTF_8));
+    }
+
 }
